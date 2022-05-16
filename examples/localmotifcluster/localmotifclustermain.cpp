7c83ab5c5ec35d875162c702b16fcb3bee392ad6
Cirus Thenter <cirusthenter@gmail.com>
2022-05-16 04:09:50 +0000

    time

--- examples/localmotifcluster/localmotifclustermain.cpp
+++ examples/localmotifcluster/localmotifclustermain.cpp
@@ -1,76 +1,80 @@
 // localmotifclustermain.cpp : Defines the entry point for the console application.
 //
-#include "stdafx.h"
 #include "localmotifcluster.h"
+#include "stdafx.h"
+#include <chrono>
+#include <iostream>
 
-int main(int argc, char* argv[]) {
-  Env = TEnv(argc, argv, TNotify::StdNotify);
-  Env.PrepArgs(TStr::Fmt("Local motif clustering. build: %s, %s. Time: %s",
-       __TIME__, __DATE__, TExeTm::GetCurTm()));  
-  TExeTm ExeTm;  
-  Try
+using namespace std;
+using namespace std::chrono;
 
-  const bool IsDirected = 
-    Env.GetIfArgPrefixBool("-d:", false, "Directed graph?");
+int main(int argc, char* argv[])
+{
+    auto start_total = high_resolution_clock::now();
+    auto start_motif = high_resolution_clock::now();
+    Env = TEnv(argc, argv, TNotify::StdNotify);
+    Env.PrepArgs(TStr::Fmt("Local motif clustering. build: %s, %s. Time: %s",
+        __TIME__, __DATE__, TExeTm::GetCurTm()));
+    TExeTm ExeTm;
+    Try
 
-  ProcessedGraph graph_p;
-  if (IsDirected) {
-    const TStr graph_filename =
-      Env.GetIfArgPrefixStr("-i:", "C-elegans-frontal.txt", "Input graph file");
-    const TStr motif =
-      Env.GetIfArgPrefixStr("-m:", "triad", "Motif type");
-    MotifType mt = ParseMotifType(motif, IsDirected);
-    PNGraph graph;
-    if (graph_filename.GetFExt().GetLc() == ".ngraph") {
-      TFIn FIn(graph_filename);
-      graph = TNGraph::Load(FIn);
-    } else if (graph_filename.GetFExt().GetLc() == ".ungraph") {
-      TExcept::Throw("Warning: input graph is an undirected graph!!");
+        const bool IsDirected
+        = Env.GetIfArgPrefixBool("-d:", false, "Directed graph?");
+
+    ProcessedGraph graph_p;
+    if (IsDirected) {
+        const TStr graph_filename = Env.GetIfArgPrefixStr("-i:", "C-elegans-frontal.txt", "Input graph file");
+        const TStr motif = Env.GetIfArgPrefixStr("-m:", "triad", "Motif type");
+        MotifType mt = ParseMotifType(motif, IsDirected);
+        PNGraph graph;
+        if (graph_filename.GetFExt().GetLc() == ".ngraph") {
+            TFIn FIn(graph_filename);
+            graph = TNGraph::Load(FIn);
+        } else if (graph_filename.GetFExt().GetLc() == ".ungraph") {
+            TExcept::Throw("Warning: input graph is an undirected graph!!");
+        } else {
+            graph = TSnap::LoadEdgeList<PNGraph>(graph_filename, 0, 1);
+        }
+        TSnap::DelSelfEdges(graph);
+        graph_p = ProcessedGraph(graph, mt);
     } else {
-      graph = TSnap::LoadEdgeList<PNGraph>(graph_filename, 0, 1);
+        const TStr graph_filename = Env.GetIfArgPrefixStr("-i:", "C-elegans-frontal.txt", "Input graph file");
+        const TStr motif = Env.GetIfArgPrefixStr("-m:", "clique3", "Motif type");
+        MotifType mt = ParseMotifType(motif, IsDirected);
+        PUNGraph graph;
+        if (graph_filename.GetFExt().GetLc() == ".ungraph") {
+            TFIn FIn(graph_filename);
+            graph = TUNGraph::Load(FIn);
+        } else if (graph_filename.GetFExt().GetLc() == ".ngraph") {
+            TExcept::Throw("Warning: input graph is a directed graph!!");
+        } else {
+            graph = TSnap::LoadEdgeList<PUNGraph>(graph_filename, 0, 1);
+        }
+        TSnap::DelSelfEdges(graph);
+        graph_p = ProcessedGraph(graph, mt);
     }
-    TSnap::DelSelfEdges(graph);
-    graph_p = ProcessedGraph(graph, mt);
 
-  } else {
+    const TInt seed = Env.GetIfArgPrefixInt("-s:", 1, "Seed");
+    const TFlt alpha = Env.GetIfArgPrefixFlt("-a:", 0.98, "alpha");
+    const TFlt eps = Env.GetIfArgPrefixFlt("-e:", 0.0001, "eps");
+    auto end_motif = high_resolution_clock::now();
+    cout << "motif discovery time: " << (double)duration_cast<microseconds>(end_motif - start_motif).count() / 1000000 << endl;
 
-    const TStr graph_filename =
-      Env.GetIfArgPrefixStr("-i:", "C-elegans-frontal.txt", "Input graph file");
-    const TStr motif =
-      Env.GetIfArgPrefixStr("-m:", "clique3", "Motif type");
-    MotifType mt = ParseMotifType(motif, IsDirected);
-    PUNGraph graph;
-    if (graph_filename.GetFExt().GetLc() == ".ungraph") {
-      TFIn FIn(graph_filename);
-      graph = TUNGraph::Load(FIn);
-    } else if (graph_filename.GetFExt().GetLc() == ".ngraph") {
-      TExcept::Throw("Warning: input graph is a directed graph!!");
-    } else {
-      graph = TSnap::LoadEdgeList<PUNGraph>(graph_filename, 0, 1);
-    }
-    TSnap::DelSelfEdges(graph);
-    graph_p = ProcessedGraph(graph, mt);
-  }
+    auto start_mappr = high_resolution_clock::now();
+    MAPPR mappr;
+    mappr.computeAPPR(graph_p, seed, alpha, eps / graph_p.getTotalVolume() * graph_p.getTransformedGraph()->GetNodes());
+    mappr.sweepAPPR(-1);
+    // mappr.printProfile();
+    printf("Size of Cluster: %d.\n", mappr.getCluster().Len());
+    auto end_mappr = high_resolution_clock::now();
+    cout << "MAPPR time: " << (double)duration_cast<microseconds>(end_mappr - start_mappr).count() / 1000000 << endl;
 
-  const TInt seed =
-    Env.GetIfArgPrefixInt("-s:", 1, "Seed");
-  const TFlt alpha =
-    Env.GetIfArgPrefixFlt("-a:", 0.98, "alpha");
-  const TFlt eps =
-    Env.GetIfArgPrefixFlt("-e:", 0.0001, "eps");
+    Catch
+        printf("\nrun time: %s (%s)\n", ExeTm.GetTmStr(),
+            TSecTm::GetCurTm().GetTmStr().CStr());
 
+    auto end_total = high_resolution_clock::now();
+    cout << "total time: " << (double)duration_cast<microseconds>(end_total - start_total).count() / 1000000 << endl;
 
-  MAPPR mappr;
-  mappr.computeAPPR(graph_p, seed, alpha, eps / graph_p.getTotalVolume() * graph_p.getTransformedGraph()->GetNodes());
-  mappr.sweepAPPR(-1);
-  mappr.printProfile();
-  printf("Size of Cluster: %d.\n", mappr.getCluster().Len());
-
-
-
-  
-  Catch
-  printf("\nrun time: %s (%s)\n", ExeTm.GetTmStr(),
-   TSecTm::GetCurTm().GetTmStr().CStr());
-  return 0;
+    return 0;
 }
