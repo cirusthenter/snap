e784959b79652c9a530ee34dca35a9edbe2052fd
Cirus Thenter <cirusthenter@gmail.com>
2022-05-25 18:48:01 +0000

    lj

--- examples/localmotifcluster/localmotifclustermain.cpp
+++ examples/localmotifcluster/localmotifclustermain.cpp
@@ -16,6 +16,20 @@
 using namespace std;
 using namespace std::chrono;
 
+// Hash function
+struct hashFunction {
+    size_t operator()(const vector<int>& myVector) const
+    {
+        std::hash<int> hasher;
+        size_t answer = 0;
+
+        for (int i : myVector) {
+            answer ^= hasher(i) + 0x9e3779b9 + (answer << 6) + (answer >> 2);
+        }
+        return answer;
+    }
+};
+
 template <class BidiIter>
 BidiIter random_unique(BidiIter begin, BidiIter end, size_t num_random)
 {
@@ -38,16 +52,34 @@ vector<unordered_set<int>> read_community(string path)
         exit(0);
     }
     vector<unordered_set<int>> communities;
+    unordered_set<vector<int>, hashFunction> nodups;
     string line;
     while (getline(file, line)) {
         unordered_set<int> community;
         istringstream ss(line);
         int num;
-        while (ss >> num)
+        vector<int> cmvec;
+        while (ss >> num) {
             community.insert(num);
-        communities.push_back(community);
+            cmvec.push_back(num);
+        }
+        sort(cmvec.begin(), cmvec.end());
+        if (nodups.find(cmvec) == nodups.end()) {
+            communities.push_back(community);
+            nodups.insert(cmvec);
+        }
     }
     return communities;
+}
+
+string convert_from_tstr(const TStr tstr)
+{
+    string s;
+    int len = tstr.Len();
+    for (int i = 0; i < tstr.Len(); ++i) {
+        s += tstr.GetCh(i);
+    }
+    return s;
 }
 
 int main(int argc, char* argv[])
@@ -61,12 +93,22 @@ int main(int argc, char* argv[])
 
     Try
 
-        const bool IsDirected
-        = Env.GetIfArgPrefixBool("-d:", false, "Directed graph?");
+        const TStr graph_filename
+        = Env.GetIfArgPrefixStr("-i:", "graph/karate_club.gr", "Input graph file");
+    const bool IsDirected = Env.GetIfArgPrefixBool("-d:", false, "Directed graph?");
+
+    // community file
+    const TStr community_filename = Env.GetIfArgPrefixStr("-c:", "community/karate.cmty.txt", "Input community file");
+    string community_path = convert_from_tstr(community_filename);
+    vector<unordered_set<int>> communities = read_community(community_path);
+
+    // output file
+    const TStr output_filename = Env.GetIfArgPrefixStr("-o:", "output/karate.output.txt", "Output file");
+    string output_path = convert_from_tstr(output_filename);
+    ofstream output(output_path);
 
     ProcessedGraph graph_p;
     if (IsDirected) {
-        const TStr graph_filename = Env.GetIfArgPrefixStr("-i:", "C-elegans-frontal.txt", "Input graph file");
         const TStr motif = Env.GetIfArgPrefixStr("-m:", "triad", "Motif type");
         MotifType mt = ParseMotifType(motif, IsDirected);
         PNGraph graph;
@@ -81,7 +123,6 @@ int main(int argc, char* argv[])
         TSnap::DelSelfEdges(graph);
         graph_p = ProcessedGraph(graph, mt);
     } else {
-        const TStr graph_filename = Env.GetIfArgPrefixStr("-i:", "C-elegans-frontal.txt", "Input graph file");
         const TStr motif = Env.GetIfArgPrefixStr("-m:", "clique3", "Motif type");
         MotifType mt = ParseMotifType(motif, IsDirected);
         PUNGraph graph;
@@ -94,8 +135,6 @@ int main(int argc, char* argv[])
             graph = TSnap::LoadEdgeList<PUNGraph>(graph_filename, 0, 1);
         }
         TSnap::DelSelfEdges(graph);
-        cout << "#nodes: " << graph->GetNodes() << ", "
-             << "#edges: " << graph->GetEdges() << endl;
         graph_p = ProcessedGraph(graph, mt);
     }
     const TFlt alpha = Env.GetIfArgPrefixFlt("-a:", 0.98, "alpha");
@@ -105,30 +144,15 @@ int main(int argc, char* argv[])
     // mappr.sweepAPPR(-1);
     // mappr.printProfile();
     auto end_motif = high_resolution_clock::now();
-    cout << "motif discovery time: " << (double)duration_cast<microseconds>(end_motif - start_motif).count() / 1000000 << endl;
+    std::cout << "motif discovery time: " << (double)duration_cast<microseconds>(end_motif - start_motif).count() / 1000000 << endl;
 
-    vector<unordered_set<int>> all_communities = read_community("community/com-orkut.top5000.cmty.txt");
-    auto rng = std::default_random_engine {};
-    std::shuffle(std::begin(all_communities), std::end(all_communities), rng);
-    std::shuffle(std::begin(all_communities), std::end(all_communities), rng);
-    std::shuffle(std::begin(all_communities), std::end(all_communities), rng);
-    vector<unordered_set<int>> communities;
-    int cnt = 0;
-    for (auto c : all_communities) {
-        if (c.size() >= 10 && c.size() <= 200) {
-            communities.push_back(c);
-            cnt++;
-            // if (cnt >= 100)
-            //     break;
-        }
-    }
-    ofstream output("output/com-orkut.top5000.cmty.output.txt");
     output << "id,precision,recall,f1" << endl;
     double sum_precision = 0, sum_recall = 0, sum_f1 = 0;
     unordered_map<int, vector<int>> nd2cluster;
     for (int i = 0; i < communities.size(); ++i) {
         auto community = communities[i];
         double max_p = 0, max_r = 0, max_f1 = 0;
+        int best_seed = -1;
         for (auto seed : community) {
             vector<int> expectations;
             if (nd2cluster.find(seed) != nd2cluster.end())
@@ -137,7 +161,7 @@ int main(int argc, char* argv[])
                 MAPPR mappr;
                 mappr.computeAPPR(graph_p, seed, alpha, eps / graph_p.getTotalVolume() * graph_p.getTransformedGraph()->GetNodes());
                 mappr.sweepAPPR(-1);
-                expectations = mappr.getNodesInOrder();
+                expectations = mappr.getNodesInOrder(graph_p, true);
                 nd2cluster[seed] = expectations;
             }
             int hit = 0;
@@ -152,23 +176,24 @@ int main(int argc, char* argv[])
                 max_p = precision;
                 max_r = recall;
                 max_f1 = f1;
+                best_seed = seed;
             }
         }
         output << i << "," << max_p << "," << max_r << "," << max_f1 << endl;
-        cout << i << "," << max_p << "," << max_r << "," << max_f1 << endl;
+        std::cout << i << "," << max_p << "," << max_r << "," << max_f1 << endl;
         sum_precision += max_p;
         sum_recall += max_r;
         sum_f1 += max_f1;
     }
     int len = communities.size();
-    cout << "FINAL precision: " << sum_precision / len << ", recall: " << sum_recall / len << ", f1: " << sum_f1 / len << endl;
+    std::cout << "FINAL precision: " << sum_precision / len << ", recall: " << sum_recall / len << ", f1: " << sum_f1 / len << endl;
 
     Catch
         printf("\nrun time: %s (%s)\n", ExeTm.GetTmStr(),
             TSecTm::GetCurTm().GetTmStr().CStr());
 
     auto end_total = high_resolution_clock::now();
-    cout << "total time: " << (double)duration_cast<microseconds>(end_total - start_total).count() / 1000000 << endl;
+    std::cout << "total time: " << (double)duration_cast<microseconds>(end_total - start_total).count() / 1000000 << endl;
 
     return 0;
 }
